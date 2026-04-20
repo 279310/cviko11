e6edf3e61c6f20ce3f379e452a57f090de26e885
279310 <279310@vutbr.cz>
2026-04-20 12:59:59 +0200

    milujem tuto animaciu

--- sorting.py
+++ sorting.py
@@ -1,15 +1,29 @@
 import random
+import matplotlib.pyplot as plt
 
 def bubble_sort(list):
     numbers = list.copy()
+    plt.ion()
+    plt.show()
     for j in range(len(list)-1):
         status = 0
         for i in range(len(list)-1-j):
             if list[i]>list[i+1]:
                 list[i], list[i+1] = list[i+1], list[i]
                 status += 1
+            index_highlight1 = i
+            index_highlight2 = i + 1
+            colors = ["steelblue"] * len(list)
+            colors[index_highlight1] = "tomato"
+            colors[index_highlight2] = "tomato"
+            plt.clf()
+            plt.bar(range(len(list)), list, color=colors)
+            plt.title("Bubble Sort")
+            plt.pause(0.1)
         if status == 0:
             break
+    plt.ioff()
+    plt.show()
     return list
 
 def selection_sort(list):
