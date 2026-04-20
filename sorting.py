8c32c53d8694588910ff3aadcbbfc15411079df7
279310 <279310@vutbr.cz>
2026-04-20 12:51:39 +0200

    bubble sort

--- sorting.py
+++ sorting.py
@@ -1,9 +1,31 @@
-def bubble_sort():
-    pass
+import random
 
-def selection_sort():
-    pass
+def bubble_sort(list):
+    numbers = list.copy()
+    for j in range(len(list)-1):
+        status = 0
+        for i in range(len(list)-1-j):
+            if list[i]>list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+                status += 1
+        if status == 0:
+            break
+    return list
 
-def random_numbers():
-    pass
+def selection_sort(list):
+    numbers = list.copy()
+    for i in range(len(list)):
+        smallest = list[i]
+        index = i
+        for j in list[i::]:
+            if j < smallest:
+                smallest = j
+                index = list.index(j)
+        list[i], list[index] = list[index], list[i]
+    return(list)
 
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+print(bubble_sort(random_numbers(10)))
